7afc54059d1e46fcbd97b30bbdf883a9b2ecdcf7
Dmitry Orlov <feanarokurufinve@mail.ru>
2017-02-16 00:27:22 +0300

    some attempt at audio playback

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,89 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_openButton_clicked()
+{
+    QString currentFileName = QFileDialog::getOpenFileName(this, tr("Open Audio"), "/home", tr("Audio Files (*.wav *.mp3 *.ogg)"));
+
+    QAudioFormat desiredFormat;
+    desiredFormat.setChannelCount(2);
+    desiredFormat.setCodec("audio/pcm");
+    desiredFormat.setSampleType(QAudioFormat::UnSignedInt);
+    desiredFormat.setSampleRate(44100);
+    desiredFormat.setSampleSize(16);
+
+    this->output = new QAudioOutput(desiredFormat, this);
+    this->device = this->output->start();
+
+    this->decoder = new QAudioDecoder(this);
+    this->decoder->setAudioFormat(desiredFormat);
+    this->decoder->setSourceFilename(currentFileName);
+
+    connect(this->decoder, SIGNAL(bufferReady()), this, SLOT(readBuffer()));
+    this->decoder->start();
+
+    ui->statusBar->showMessage("File " + currentFileName + " is loaded", 3000);
+}
+
+void MainWindow::readBuffer() {
+    //here we put the logic to execute when we read a chunk of data
+}
+
+
+void MainWindow::on_playButton_clicked()
+{
+
+}
+
+void MainWindow::on_verticalSlider_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 1 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_2_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 2 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_3_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 3 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_4_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 4 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_5_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 5 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_6_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 6 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_7_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 7 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_8_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 8 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_9_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 9 moved to " + std::to_string(position)), 3000);
+}
+
+void MainWindow::on_verticalSlider_10_sliderMoved(int position)
+{
+    ui->statusBar->showMessage(QString::fromStdString("Slider 10 moved to " + std::to_string(position)), 3000);
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QAudioDecoder>
+#include <QAudioFormat>
+#include <QAudioOutput>
+#include <QQueue>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,45 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_openButton_clicked();
+
+    void on_playButton_clicked();
+
+    void on_verticalSlider_sliderMoved(int position);
+
+    void on_verticalSlider_2_sliderMoved(int position);
+
+    void on_verticalSlider_3_sliderMoved(int position);
+
+    void on_verticalSlider_4_sliderMoved(int position);
+
+    void on_verticalSlider_5_sliderMoved(int position);
+
+    void on_verticalSlider_6_sliderMoved(int position);
+
+    void on_verticalSlider_7_sliderMoved(int position);
+
+    void on_verticalSlider_8_sliderMoved(int position);
+
+    void on_verticalSlider_9_sliderMoved(int position);
+
+    void on_verticalSlider_10_sliderMoved(int position);
+
+    void readBuffer();
+
 private:
     Ui::MainWindow *ui;
+
+    QString currentFileName;
+
+    QAudioDecoder *decoder;
+
+    QIODevice *device;
+
+    QAudioOutput *output;
+
+    QByteArray *bytes;
 };
 
 #endif // MAINWINDOW_H
